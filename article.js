f343015fa25cc33923feda7c1a12e45d3ab4d7ad
Armibule <armand.gautry@gmail.com>
2024-12-04 16:19:18 +0100

    Ajout de la modification de la résolution des couleurs + autre

--- article.js
+++
@@ -1,6 +0,0 @@
-
-
-var buttonToTop = document.querySelector(".articleToTop")
-buttonToTop.onclick = function() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-}
--- imageMaker.js
+++ imageMaker.js
@@ -1,4 +1,7 @@
 const loadImageButton = document.getElementById("loadImageButton")
+const imageLink = document.getElementById("imageLink")
+const imageLinkInput = document.getElementById("imageLinkInput");
+
 const loadedImage = document.getElementById("loadedImage")
 const loadImagePopup = document.querySelector(".loadImagePopup")
 
@@ -23,22 +26,62 @@ const fileInput = document.createElement("input")
 fileInput.type = "file"
 fileInput.accept = "image/*"
 
-
 var code = ""
+
+// Setup parameters and listeners
+// 1 is not possible
+var scale = 2
+
+// in bits, minimum is 2, max is 8
+var colorFoldPower = 5
+var colorFold = 2**colorFoldPower
+
+const resolutionInput = document.getElementById("resolutionInput");
+const colorsInput = document.getElementById("colorsInput");
+resolutionInput.oninput = () => {
+    scale = 4 - resolutionInput.value;
+    if (code.length > 0) {
+        updateImage();
+    }
+}
+colorsInput.oninput = () => {
+    colorFoldPower = 7-colorsInput.value;
+    colorFold = 2**colorFoldPower;
+    if (code.length > 0) {
+        updateImage();
+    }
+}
+resolutionInput.oninput()
+colorsInput.oninput()
 
 
 function openloadImagePopup() {
     loadImagePopup.classList.add("openedLoadImagePopup")
+    uploadImageButton.style.display = "inherit"
+    imageLink.style.display = "inherit"
+    imageLinkInput.style.display = "none"
 }
 function closeloadImagePopup() {
     loadImagePopup.classList.remove("openedLoadImagePopup")
 }
 
 loadImageButton.onclick = openloadImagePopup
+document.addEventListener("click", (event) => {
+    if (event.target == loadImagePopup) {
+        // is the background of the popup
+        closeloadImagePopup()
+    }
+})
 
 uploadImageButton.onclick = function() {
     fileInput.click()
 }
+imageLink.onclick = function() {
+    uploadImageButton.style.display = "none"
+    imageLink.style.display = "none"
+    imageLinkInput.style.display = "block"
+}
+
 
 copyCodeButton.onclick = function() {
     navigator.clipboard.writeText(code)
@@ -53,12 +96,7 @@ function setPixel(x, y, color) {
     imageCanvasCtx.fillRect(x, y, 1, 1)
 }
 
-// 1 is not possible
-var scale = 2
 
-// in bits, minimum is 2, max is 8
-var colorFoldPower = 5
-var colorFold = 2**colorFoldPower
 function foldValue(value) {
     // return Math.round(value/colorFold)*colorFold
     return Math.round(value / colorFold)
@@ -102,26 +140,7 @@ const indexesLabels = `{zEDGFA@CBMLONIHKJUTWVQPSR]_^YX[Z%̈$'&! #"-,/.̄)(+*
     /*# s="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ,;:!/*-+._={}[]()<>@$&%^|\"☺☻♥♦♣♠•◘○◙♂♀♪♫☼►◄↕¶§▬↨↑↓→←∟↔▲▼┤©╣║╝¢¥┐└┴┬├─┼╚ı┘┌█▄¦▀ß"
 s="".join([chr(i) for i in range(256)])
 s="""¡¢£¤¥¦§̈©«¬­®̄°±%$'&! #"-,/.)(+*54761032=<?>98;:
-edgfa`cbmlonihkjutwvqpsr}|~yx{zEDGFA@CBMLONIHKJUTWVQPSR]\_^YX[Z"""
-a = set(s)
-
-#for i in s:
-  #print(i)
-  
-print(a)
-
-for i in a:
-  print(i)
-
-if len(a) != len(s):
-  print(len(s) - len(a))
-  for i in a:
-    if s.count(i) > 1:
-      print(i, s.count(i))
-  print("".join(a))
-else:
-  print("OK")
-  print(s)*/
+edgfa`cbmlonihkjutwvqpsr}|~yx{zEDGFA@CBMLONIHKJUTWVQPSR]\_^YX[Z"""*/
 
 function pixelsToCodeIndexed(data, width, height, partitionLength) {
     if (indexesLabels.length < partitionLength || partitionLength <= 0) {
@@ -140,7 +159,7 @@ function pixelsToCodeIndexed(data, width, height, partitionLength) {
             color = getRGB(data, width, x, y)
             key = colorToKey(color)
             if (!(key in indexed)) {
-                console.log(color[0]*colorFold, color[1]*colorFold, color[2]*colorFold)
+                // console.log(color[0]*colorFold, color[1]*colorFold, color[2]*colorFold)
 
                 indexed[key] = [color, indexesLabels[indexedCount]] // TODO: chose most important colors
                 indexedCount += 1
@@ -154,14 +173,14 @@ function pixelsToCodeIndexed(data, width, height, partitionLength) {
         }
     }
 
-    console.log(indexed)
+    // console.log(indexed)
     console.log(indexedCount)
 
-    var pixels = ""
-    var label = ""
-    var lastLabel = ""
-    var repetitionCount = 0
-    var color
+    var pixels = "";
+    var label = "";
+    var lastLabel = "";
+    var repetitionCount = 0;
+    var color;
     for (y=0 ; y<height ; y++) {
         for (x=0 ; x<width ; x++) {
             color = getRGB(data, width, x, y)
@@ -202,6 +221,60 @@ function pixelsToCodeIndexed(data, width, height, partitionLength) {
     return result
 }
 
+function updateImage() {
+    const maxWidth = 320/scale
+    const maxHeight = 220/scale
+
+    width = loadedImage.naturalWidth
+    height = loadedImage.naturalHeight
+
+    if (width > maxWidth) {
+        height *= maxWidth/width
+        width = maxWidth
+    }
+    if (height > maxHeight) {
+        width *= maxHeight/height
+        height = maxHeight
+    }
+    width = Math.round(width)
+    height = Math.round(height)
+
+    imageCanvas.width = width
+    imageCanvas.height = height
+
+    imageCanvasCtx.imageSmoothingEnabled = false
+
+    imageCanvasCtx.drawImage(loadedImage, 0, 0, loadedImage.naturalWidth, loadedImage.naturalHeight, 0, 0, width, height)
+    var data = imageCanvasCtx.getImageData(0, 0, imageCanvas.width, imageCanvas.height).data
+    code = pixelsToCodeIndexed(data, imageCanvas.width, imageCanvas.height, indexesLabels.length)
+
+    var codeLength = code.length
+
+    if (codeLength < 1000) {
+        resultStorage.innerText = `${codeLength} octets`
+    } else {
+        resultStorage.innerText = `${Math.round(codeLength/10)/100} Ko`
+
+        if (codeLength > 12000) {
+            resultStorage.innerText += " ⚠"
+        }
+    }
+
+    resultStorage.classList.remove("resultStorageSmall")
+    resultStorage.classList.remove("resultStorageMedium")
+    resultStorage.classList.remove("resultStorageBig")
+    if (codeLength < 4000) {
+        resultStorage.classList.add("resultStorageSmall")
+    } else if (codeLength < 8000) {
+        resultStorage.classList.add("resultStorageMedium")
+    } else {
+        resultStorage.classList.add("resultStorageBig")
+    }
+
+    resultWidth.innerText = width
+    resultHeight.innerText = height
+}
+
 fileInput.onchange = function(e) {
     var reader = new FileReader()
     reader.readAsDataURL(e.target.files[0])
@@ -213,56 +286,22 @@ fileInput.onchange = function(e) {
        loadedImage.src = content
 
        loadedImage.onload = function() {
-            const maxWidth = 320/scale
-            const maxHeight = 220/scale
-
-            width = loadedImage.naturalWidth
-            height = loadedImage.naturalHeight
-
-            if (width > maxWidth) {
-                height *= maxWidth/width
-                width = maxWidth
-            }
-            if (height > maxHeight) {
-                width *= maxHeight/height
-                height = maxHeight
-            }
-            width = Math.round(width)
-            height = Math.round(height)
-
-            imageCanvas.width = width
-            imageCanvas.height = height
-
-            imageCanvasCtx.imageSmoothingEnabled = false
-
-            imageCanvasCtx.drawImage(loadedImage, 0, 0, loadedImage.naturalWidth, loadedImage.naturalHeight, 0, 0, width, height)
-            var data = imageCanvasCtx.getImageData(0, 0, imageCanvas.width, imageCanvas.height).data
-            code = pixelsToCodeIndexed(data, imageCanvas.width, imageCanvas.height, indexesLabels.length)
-
-            var codeLength = code.length
-
-            if (codeLength < 1000) {
-                resultStorage.innerText = `${codeLength} octets`
-            } else {
-                resultStorage.innerText = `${Math.round(codeLength/10)/100} Ko`
-            }
-
-            resultStorage.classList.remove("resultStorageSmall")
-            resultStorage.classList.remove("resultStorageMedium")
-            resultStorage.classList.remove("resultStorageBig")
-            if (codeLength < 4000) {
-                resultStorage.classList.add("resultStorageSmall")
-            } else if (codeLength < 8000) {
-                resultStorage.classList.add("resultStorageMedium")
-            } else {
-                resultStorage.classList.add("resultStorageBig")
-            }
-
-            resultWidth.innerText = width
-            resultHeight.innerText = height
+            updateImage();
        }
     }
 
    closeloadImagePopup()
 }
+imageLinkInput.addEventListener("keydown", (event) => {
+    if (event.key == "Enter") {
+        loadedImage.src = imageLinkInput.value
 
+        loadedImage.onload = function() {
+            updateImage();
+        }
+        loadedImage.onerror = function() {
+            console.log("Image failed to load...")
+        }
+        closeloadImagePopup()
+    }
+})
